df210c67f8a392985425e99ebb1bf5e492e88984
9-max-6 <mutukumaxwel@gmail.com>
2024-08-28 16:03:23 +0300

    Chore: attempted the second question in the series.

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """module: function: filter_datum"""
 import re
+import logging
 from typing import List
 
 
@@ -24,3 +25,26 @@ def filter_datum(
         lambda match: match.group(0).split("=")[0]+"="+f"{redaction}"
         ), p) for p in ps]
     return separator.join(ob_parts)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """a function to implement redaction"""
+        original_message = super().format(record)
+        return filter_datum(
+            self.fields,
+            self.REDACTION,
+            original_message,
+            self.SEPARATOR
+            )
