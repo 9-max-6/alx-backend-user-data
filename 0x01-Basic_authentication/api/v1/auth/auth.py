a4f9ed934fb8e1dc7a3839d8811e4bc846a31cc9
9-max-6 <mutukumaxwel@gmail.com>
2024-09-03 14:45:24 +0300

    Fix: fixed pycodestyle issues

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -4,6 +4,7 @@ from flask import request
 from typing import List, TypeVar
 from models.user import User
 
+
 class Auth:
     """a class to wrap auth logic"""
 
@@ -17,12 +18,12 @@ class Auth:
             path = path + '/'
             if path in excluded_paths:
                 return False
-        
+
     def authorization_header(self, request=None) -> str:
         """returns None"""
 
         return None
-    
+
     def current_user(self, request=None) -> TypeVar('User'):
         """a function that returns None"""
         return None
