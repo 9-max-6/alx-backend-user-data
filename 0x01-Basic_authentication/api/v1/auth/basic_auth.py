3fa5859ee1eceb4d4b1559d5727580616cc6aa61
9-max-6 <mutukumaxwel@gmail.com>
2024-09-03 19:23:16 +0300

    Fix: changed the function name to bs64decode()

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 """Basic auth module"""
+import base64
+import binascii
 from api.v1.auth.auth import Auth
 
 
@@ -20,3 +22,18 @@ class BasicAuth(Auth):
         if not authorization_header.startswith(prefix):
             return None
         return authorization_header[len(prefix):]
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str
+            ) -> str:
+        """A function to decode the Base64 string"""
+        if not base64_authorization_header:
+            return None
+        if type(base64_authorization_header) is not str:
+            return None
+        try:
+            decoded_data = base64.b64decode(base64_authorization_header)
+            return decoded_data.decode('utf-8')
+        except binascii.Error as e:
+            return None
