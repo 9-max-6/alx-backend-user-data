ec79ca6dbbdb0a515d9c6325c5ac4d60f892d052
9-max-6 <mutukumaxwel@gmail.com>
2024-09-03 16:56:26 +0300

    Chore: moved the new class to the right file. <basic_auth.py>

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Basic auth module"""
+from auth import Auth
+
+
+class BasicAuth(Auth):
+    """Empty class"""
+    def extract_base64_authorization_header(
+            self,
+            authorization_header: str
+            ) -> str:
+        """returns the Base64 part of the Authorization
+        header for a Basic Authentication:
+        """
+        if not authorization_header:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        prefix = 'Basic '
+        if not authorization_header.startswith(prefix):
+            return None
+        return authorization_header[len(prefix):]
