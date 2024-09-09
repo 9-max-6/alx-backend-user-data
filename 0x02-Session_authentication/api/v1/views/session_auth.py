3cbe8644303357ca56bec3b9bb36d962b5188bce
9-max-6 <mutukumaxwel@gmail.com>
2024-09-09 16:46:10 +0300

    Chore: the 8th question in the project.

---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+""" Module of Index views
+"""
+import os
+from flask import jsonify, abort, request
+from api.v1.views import app_views
+from models.user import User
+
+@app_views.route(
+        '/auth_session/login',
+        methods=['POST'],
+        strict_slashes=False
+        )
+def login() -> str:
+    """a function to implement login logic"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if not email:
+        return ({
+            "error": "email missing"
+        }), 400
+
+    if not password:
+        return jsonify(
+            {
+            "error": "password missing"
+        }), 400
+    
+    user = User.search({"email": email})
+    if not user:
+        return jsonify(
+            {
+                "error": "no user found for this email"
+            }
+        ), 404
+    
+    if not user[0].is_valid_password(password):
+        return jsonify(
+            {
+                "error": "wrong password"
+            }
+        ), 401
+    
+    from api.v1.app import auth
+    session_id = auth.create_session(user[0].id)
+    cookie_name = os.getenv("SESSION_NAME")
+    if cookie_name:
+        resp = jsonify(user[0].to_json())
+        resp.set_cookie(cookie_name, session_id)
+        return resp
