8e2b152f5470a0acae1958429766b078f41fc44c
9-max-6 <mutukumaxwel@gmail.com>
2024-09-12 17:06:26 +0300

    Fix: added try except block to catch result not found error when checking for the presence of a user before adding

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,6 +3,7 @@
 """
 import bcrypt
 from db import DB
+from sqlalchemy.orm.exc import NoResultFound
 from user import User
 
 
@@ -15,12 +16,14 @@ class Auth:
 
     def register_user(self, email: str, password: str) -> User:
         """A function to register a user"""
-        if self._db.find_user_by(email=email):
-            raise ValueError(f"User {email} already exists")
-
+        try:
+            if self._db.find_user_by(email=email):
+                raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            pass
         new_user = self._db.add_user(email, _hash_password(password))
         if new_user:
-            return
+            return new_user
 
 
 def _hash_password(password: str) -> str:
