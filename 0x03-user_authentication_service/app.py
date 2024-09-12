ee451ada812fd02f9b6ff6ac01b6a8074c23c7a5
9-max-6 <mutukumaxwel@gmail.com>
2024-09-12 17:20:49 +0300

    Chore: attempted the 8th question inthe project.

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 """Flask app"""
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
+
+AUTH = Auth()
 
 
 app = Flask(__name__)
@@ -14,6 +17,25 @@ def hello() -> str:
             "message": "Bienvenue"
         }
     )
+
+
+@app.route('/users', methods=["POST"])
+def users() -> str:
+    """registers a user"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if not email and not password:
+        pass
+    try:
+        AUTH.register_user(email, password)
+        return jsonify(
+            {"email": email, "message": "user created"}
+        )
+    except ValueError:
+        return jsonify(
+            {"message": "email already registered"}
+        ), 400
 
 
 if __name__ == "__main__":
