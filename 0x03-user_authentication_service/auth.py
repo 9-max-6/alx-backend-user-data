cd522aa25429f0090b7844be19c7568e58465875
9-max-6 <mutukumaxwel@gmail.com>
2024-09-12 21:01:06 +0300

    Chore: added the reset passoword token function

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -85,6 +85,24 @@ class Auth:
 
         return None
 
+    def get_reset_password_token(self, email: str) -> str:
+        """
+        a function to generate reset token
+        Args:
+        email - the email of the user whose reset token is to be set
+
+        Returns:
+        if the user is found, the function sets a token in the database
+        for the user
+        else: raise a ValueError
+        """
+        user = self._db.find_user_by(email=email)
+        if not user:
+            raise ValueError
+        reset_token = _generate_uuid()
+        self._db.update_user(user.id, reset_token=reset_token)
+        return reset_token
+
 
 def _hash_password(password: str) -> str:
     """returns a hashed password"""
