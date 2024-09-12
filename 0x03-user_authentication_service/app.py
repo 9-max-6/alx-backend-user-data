95e7839c235df14081752d27f724a29df251f617
9-max-6 <mutukumaxwel@gmail.com>
2024-09-12 20:06:11 +0300

    Chore: attemoted the login session task of the project.

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Flask app"""
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 AUTH = Auth()
@@ -36,6 +36,25 @@ def users() -> str:
         return jsonify(
             {"message": "email already registered"}
         ), 400
+
+
+@app.route('/sessions', methods=["POST"])
+def login() -> str:
+    """a function to implement the login login"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if not AUTH.valid_login(email, password):
+        abort(401)
+
+    session_id = AUTH.create_session(email)
+    payload = {"email": email,
+               "message": "logged in"
+               }
+    response = jsonify(payload)
+    response.set_cookie("session_id", session_id)
+
+    return response
 
 
 if __name__ == "__main__":
