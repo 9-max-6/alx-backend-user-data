5bac5fb940571db76247034d56e068112d87873e
9-max-6 <mutukumaxwel@gmail.com>
2024-09-12 17:00:17 +0300

    Chore: added the Auth class and register_usr() method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,7 +2,25 @@
 """DB module
 """
 import bcrypt
+from db import DB
+from user import User
 
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+    
+    def register_user(self, email: str, password: str) -> User:
+        """A function to register a user"""
+        if self._db.find_user_by(email=email):
+            raise ValueError(f"User {email} already exists")
+
+        new_user = self._db.add_user(email, _hash_password(password))
+        if new_user:
+            return
 
 def _hash_password(password: str) -> str:
     """returns a hashed password"""
